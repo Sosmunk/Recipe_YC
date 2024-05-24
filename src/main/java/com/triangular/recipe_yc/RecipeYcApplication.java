70e140ac31e4a6633239d6b46b5b1af9534ccbe6
andrey.m <andrei.mesilov@urfu.me>
2024-05-24 23:36:56 +0500

    init

---
+++ src/main/java/com/triangular/recipe_yc/RecipeYcApplication.java
@@ -0,0 +1,13 @@
+package com.triangular.recipe_yc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeYcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeYcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/triangular/recipe_yc/RecipeYcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.triangular.recipe_yc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeYcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
