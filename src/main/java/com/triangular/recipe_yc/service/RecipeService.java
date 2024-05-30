7dcd04c839be7cc4a6da3e9aa319f6db4df6a3ca
ainlorn <ap4khomov@gmail.com>
2024-05-30 15:37:31 +0500

    add authorize annotations

--- src/main/java/com/triangular/recipe_yc/service/RecipeService.java
+++ src/main/java/com/triangular/recipe_yc/service/RecipeService.java
@@ -1,19 +1,22 @@
 package com.triangular.recipe_yc.service;
 
 import com.triangular.recipe_yc.dto.RecipeInfo;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface RecipeService {
-
+    @PreAuthorize("isAuthenticated()")
     RecipeInfo saveRecipe(RecipeInfo recipeInfo, MultipartFile picture);
 
     RecipeInfo getRecipe(UUID recipeId);
 
+    @PreAuthorize("isAuthenticated()")
     RecipeInfo editRecipe(UUID recipeId, RecipeInfo recipeInfo, MultipartFile picture);
 
+    @PreAuthorize("isAuthenticated()")
     void deleteRecipe(UUID recipeId);
 
     List<RecipeInfo> getRecipeList();
